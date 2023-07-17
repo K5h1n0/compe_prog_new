3ae825140d5cb9edaa62aae342387371d5d99f21
K5h1n0 <shinokou1001@gmail.com>
2023-07-18 06:39:17 +0900

    solved!

--- ABC240/c/main.py
+++ ABC240/c/main.py
@@ -1,16 +1,28 @@
-# DPでした
 n,x = map(int,input().split())
-lb = []
-l_sa = []
-suma = 0
-sumb = 0
+length = 100001
+l1 = [0]*length
+l2 = [0]*length
 for i in range(n):
     a,b = map(int,input().split())
-    suma += a
-    sumb += b
-    lb.append(b)
-    l_sa.append(b-a)
-if x < suma or sumb < x:
+    if i % 2:
+        l2 = [0]*length
+        for j in range(length):
+            if l1[j] == 1:
+                l2[j+a] = 1
+                l2[j+b] = 1    
+    else:
+        if i == 0:
+            l1[a] = 1
+            l1[b] = 1
+            continue
+        l1 = [0]*length
+        for j in range(length):
+            if l2[j] == 1:
+                l1[j+a] = 1
+                l1[j+b] = 1
+if n%2 and l1[x]:
+    print("Yes")
+elif n%2 == 0 and l2[x]:
+    print("Yes")
+else:
     print("No")
-    exit()
-
